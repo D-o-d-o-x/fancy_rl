ebff2551cded3d7702d1ec2ebc01f09a242a95e5
Dominik Roth <dominik.roth.dev@gmail.com>
2024-08-28 11:34:26 +0200

    projections_legacy no longer requierd

--- fancy_rl/projections_legacy/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    import cpp_projection
-except ModuleNotFoundError:
-    from .base_projection_layer import ITPALExceptionLayer as KLProjectionLayer
-else:
-    from .kl_projection_layer import KLProjectionLayer
--- fancy_rl/projections_legacy/identity_projection_layer.py
+++
@@ -1,5 +0,0 @@
-from .base_projection_layer import BaseProjectionLayer
-
-class IdentityProjectionLayer(BaseProjectionLayer):
-    def project_from_rollouts(self, dist, rollout_data, **kwargs):
-        return dist, dist
